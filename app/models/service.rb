8a5399af920cce2688ca667e0c482fcef6a4895e
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-06-25 08:55:24 -0600

    Validate logo_url.

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,7 @@
 class Service < ActiveRecord::Base
   has_many :apps, inverse_of: :service
 
+  validates_format_of :logo_url, with: URI::regexp(%w(http https))
   validates :name, uniqueness: { case_sensitive: false }
   validates :name, presence: true
 end
