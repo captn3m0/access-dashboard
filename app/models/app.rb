b0490032e1ccd77fdcebe5a25a6062de82c8d95e
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-06-24 18:17:34 -0600

    Enforce uniqueness of apps and services.

--- app/models/app.rb
+++ app/models/app.rb
@@ -2,4 +2,5 @@ class App < ActiveRecord::Base
   belongs_to :service
 
   validates :service, :name, presence: true
+  validates :name, uniqueness: { scope: :service, case_sensitive: false }
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,6 @@
 class Service < ActiveRecord::Base
   has_many :apps, inverse_of: :service
 
+  validates :name, uniqueness: { case_sensitive: false }
   validates :name, :is_dynamic, presence: true
 end
