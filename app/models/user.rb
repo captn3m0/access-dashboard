1fd7194458b9f58304f7c4fc622f5f8534e08166
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-07-02 11:52:13 -0600

    Allow username to be nil.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   has_many :services, through: :identities
   has_and_belongs_to_many :apps
 
-  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validates :username, uniqueness: { case_sensitive: false }, allow_nil: true
   validates_format_of :avatar_url, with: URI::regexp(%w(http https)), allow_blank: true
 end
