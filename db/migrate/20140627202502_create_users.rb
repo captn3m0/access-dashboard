48380708b7df269329fe701990daa49f34739fe6
Patrick Connolly <patrick.c.connolly@gmail.com>
2014-07-02 12:00:38 -0600

    Add heroku_email field.

--- db/migrate/20140627202502_create_users.rb
+++ db/migrate/20140627202502_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :avatar_url
+      t.string :heroku_email
 
       t.timestamps
     end
